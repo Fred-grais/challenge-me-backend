e6947965e003fb8286990cc2443545d79cd270c4
Fred Grais <frederic.grais@gmail.com>
2018-09-13 01:10:40 +0200

    Using ENV variable for devise secret token

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,6 +1,6 @@
 Devise.setup do |config|
 
 
-  config.secret_key = 'a2e6c97c118ea3753e02484fa6e99dcb088003f0b3a6e2e890c9ef8aabadeb73eb6c11a7e0e06f211117b13eacdd439eeb61f7eb85df96611d4f384ce56e83c2'
+  config.secret_key = ENV['SECRET_KEY_BASE']
 
 end
